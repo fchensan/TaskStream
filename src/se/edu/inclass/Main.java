68938f4c7a2f3dbd6fd2460966eabc0185ca1802
Francisco Chensan <chensan@u.nus.edu>
2020-09-18 17:13:45 +0800

    Add printDeadlinesUsingStreams

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -49,7 +49,7 @@ public class Main {
             System.out.println(t);
         }
     }
-    
+
     public static void printDataUsingStreams(ArrayList<Task> tasksData){
         System.out.println("Printing data using streams");
         tasksData.stream()
@@ -62,5 +62,12 @@ public class Main {
                 System.out.println(t);
             }
         }
+    }
+
+    public static void printDeadlinesUsingStreams(ArrayList<Task> tasksData) {
+        System.out.println("Printing deadlines using streams");
+        tasksData.stream()
+                .filter((t) -> t instanceof Deadline)
+                .forEach(System.out::println);
     }
 }
